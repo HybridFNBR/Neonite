486069f2f078c04f1f7e79c438561db804ef9b38
Joshua <68912574+HybridFNBR@users.noreply.github.com>
2024-01-12 01:07:27 +0000

    oauth.js

--- managers/oauth.js
+++ managers/oauth.js
@@ -27,46 +27,35 @@ module.exports = (app) => {
 
 				accountId = displayName.replace(/ /g, "_");
 				break;
-
 			}
 			let token = jsonwebtoken.sign({
-				"app": "prod-fn",
-				"sub": accountId,
-				"dvid": "89776e294d5c27ba1ef4e59fab402ea7",
-				"mver": false,
-				"clid": accountId,
-				"dn": displayName,
-				"am": "exchange_code",
-				"pfpid": "prod-fn",
-				"iai": accountId,
-				"sec": 1,
-				"acr": "urn:epic:loa:aal1",
+				"p": "eNqtk8lOAzEMht+nQkhlO1iaA0tBnEBC4joyiWdqNeNUiVPo2+NhGShLBxCnbF7+/0vSxKTCSuBCLD5rTNgS5HVW6uCcUEsif5kDij/Y5aexmh7tND9P2x9NK5kSuCgNt9Xe9tK3i5lnPSPPDpX8DaUVpQvsaJeFR4XdLq4DrrdkY9NwYDuDZbkL7GDYGBE2pvtfFc+kZRnyAxZxcyPo472EiB4CrwgmJilHxxhACbsMkqGR6mjHCmGILeQ52h1BbBpK2YI/15nA+Yu20yhKoieFg+9j0blYRAdKz8tq+isImzZeS0YsOgd60Pppck+tsYKEHOpMOXOUWtktSKvD7d16AFQCakK3YBkM2w5lxTYRdebps5u2YPKMks3P10Z7eZQEw7FJvJKwtsgPWCfv6r6M2YB2pOgtEubLun/2Nft6maL/07vfBPiLLzl99yVNxIodsRgTcfQNtAHX3doa97cwpniz495bx0dUELK5",
 				"clsvc": "prod-fn",
 				"t": "s",
-				"auth_time": new Date(new Date().getTime().toString()),
+				"mver": false,
+				"clid": "ec684b8c687f479fadea3cb2ad83f5c6",
 				"ic": true,
-				"exp": 1704980387,
-				"iat": 1704973187,
-				"jti": "6b74350c915342569b5e73cc1645f391"
-			  }, "PS256");
-			  
+				"exp": 2147483647,
+				"am": "client_credentials",
+				"iat": 1705018783,
+				"jti": "c01f29504dcd42f9b68cf55759392928",
+				"pfpid": "prod-fn"
+			  }, "PS256", {keyid:""})		  
 			res.json({
-				access_token: `eg1~${token}`,
-				expires_in: 28800,
-				expires_at: "9999-12-31T23:59:59.999Z",
-				token_type: "bearer",
-				account_id: accountId,
-				client_id: "NeoniteV2Client",
-				internal_client: true,
-				client_service: "fortnite",
-				refresh_token: `eg1~${token}`,
-				refresh_expires: 115200,
-				refresh_expires_at: "9999-12-31T23:59:59.999Z",
-				displayName: displayName,
-				app: "fortnite",
-				in_app_id: accountId,
-				device_id: "5dcab5dbe86a7344b061ba57cdb33c4f"
+				"access_token": `eg1~${token}`,
+				"displayName": displayName,
+				"account_id": accountId,
+				"expires_in": 2147483647,
+				"expires_at": "9999-12-31T23:59:59.999Z",
+				"token_type": "bearer",
+				"refresh_token":`eg1~${token}`,
+				"client_id": "ec684b8c687f479fadea3cb2ad83f5c6",
+				"internal_client": true,
+				"client_service": "prod-fn",
+				"product_id": "prod-fn",
+				"application_id": "fghi4567FNFBKFz3E4TROb0bmPS8h1GW"
 			})
+			res.status(200);
 		})
 	
 	//verify token
@@ -74,22 +63,34 @@ module.exports = (app) => {
 		const JWT = req.headers.authorization.replace("bearer eg1~", "")
 		const JWTdecode = jsonwebtoken.decode(JWT)
 		res.json({
-			access_token: req.headers.authorization.replace("bearer ", ""),
-			expires_in: 28800,
-			expires_at: "9999-12-31T23:59:59.999Z",
-			token_type: "bearer",
-			refresh_token: req.headers.authorization.replace("bearer ", ""),
-			refresh_expires: 115200,
-			refresh_expires_at: "9999-12-31T23:59:59.999Z",
-			account_id: JWTdecode["sub"],
-			client_id: "NeoniteV2Client",
-			internal_client: true,
-			client_service: "fortnite",
-			displayName: JWTdecode["sub"],
-			app: "fortnite",
-			in_app_id: JWTdecode["sub"],
-			device_id: "5dcab5dbe86a7344b061ba57cdb33c4f"
-		})
+			"token": req.headers.authorization,
+			"session_id": "NEONITESESSION",
+			"token_type": "bearer",
+			"client_id": "ec684b8c687f479fadea3cb2ad83f5c6",
+			"internal_client": true,
+			"client_service": "prod-fn",
+			"account_id": JWTdecode["sub"],
+			"expires_in": 2147483647,
+			"expires_at": "2024-01-12T02:20:15.951Z",
+			"auth_method": "exchange_code",
+			"display_name": JWTdecode["sub"],
+			"app": "prod-fn",
+			"in_app_id": "e7c388664e5442e89f30b396d9fa7183",
+			"device_id": "89776e294d5c27ba1ef4e59fab402ea7",
+			"scope": [
+			  "basic_profile",
+			  "friends_list",
+			  "openid",
+			  "presence"
+			],
+			"product_id": "prod-fn",
+			"sandbox_id": "fn",
+			"deployment_id": "62a9473a2dca46b29ccf17577fcf42d7",
+			"application_id": "fghi4567FNFBKFz3E4TROb0bmPS8h1GW",
+			"acr": "urn:epic:loa:aal1",
+			"auth_time": "1999-01-12T00:20:15.542Z"
+		  })
+		res.status(200);
 	});
 
 	//kill token
