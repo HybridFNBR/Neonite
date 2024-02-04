3989c0a12761e903fe38133731fa99ae0c1d26d8
Joshua <68912574+HybridFNBR@users.noreply.github.com>
2024-02-04 18:33:36 +0000

    added url logging back.

---
+++ config/http.js
@@ -0,0 +1,27 @@
+const NeoLog = require("../structs/NeoLog");
+module.exports.http = {
+    middleware: {
+      order: [
+        'LogURL',   
+        'bodyParser'   
+      ],
+      LogURL: function (req, res, next) {
+        const startTime = new Date();
+
+        res.on('finish', () => {
+          const endTime = new Date();
+          const responseTime = endTime - startTime;
+  
+          if (req.originalUrl === "/fortnite/api/calendar/v1/timeline" || req.originalUrl.match(/^\/ias\/fortnite\//)) {
+          } 
+          else 
+          {
+            NeoLog.URL(`${req.originalUrl} (${responseTime}ms)`);
+          }
+        });
+        next();
+        },
+        bodyParser: require('skipper')({ strict: true }),
+    
+    }
+}
