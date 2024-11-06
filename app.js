2703b42af14314eed01a4f6058e5a747b01f78b5
Joshua <68912574+HybridFNBR@users.noreply.github.com>
2024-11-06 14:11:59 +0000

    logically better to use nitestats then fncentral for the keychain

--- app.js
+++ app.js
@@ -5,45 +5,26 @@ const fs = require("fs");
 
 
 async function compareAndUpdateKeychain() {
-	const response = await axios.get('https://fortnitecentral.genxgames.gg/api/v1/aes');
-	if(response.status = 200){
-		const data = response.data;
-		const keychain = JSON.parse(fs.readFileSync('./responses/keychain.json'));
-		let count = 0;
-		const newEntries = [];
-		for (const entry of data["dynamicKeys"]) {
-			if (!keychain.includes(entry["keychain"])) {
-				count++;
-				newEntries.push(entry["keychain"]);
-			}
+	const response = await axios.get('https://api.nitestats.com/v1/epic/keychain');
+	const data = response.data;
+	const localData = JSON.parse(fs.readFileSync('./responses/keychain.json'));
+	let count = 0
+	for (const entry of data) {
+		if (!localData.includes(entry)) {
+			count++
+			localData.push(entry);
 		}
-		keychain.push(...newEntries);
-		fs.writeFileSync('./responses/keychain.json', JSON.stringify(keychain, null, 2));
-		NeoLog.Debug(`Fetched ${count} New Keychains From Fortnite Central`)
 	}
-	else if(response.status == 503 || response.status == 403){
-		NeoLog.Error("Fortnite Central is down falling back to Nitestats for the keychain")
-		const response = await axios.get('https://api.nitestats.com/v1/epic/keychain');
-	  	const data = response.data;
-		const localData = JSON.parse(fs.readFileSync('./responses/keychain.json'));
-		let count = 0
-		for (const entry of data) {
-			if (!localData.includes(entry)) {
-				count++
-				localData.push(entry);
-			}
-		}
-		fs.writeFileSync('./responses/keychain.json', JSON.stringify(localData));
-		fs.readFile('./responses/keychain.json', 'utf8', (err, data) => {
-			if (err) throw err;
+	fs.writeFileSync('./responses/keychain.json', JSON.stringify(localData));
+	fs.readFile('./responses/keychain.json', 'utf8', (err, data) => {
+		if (err) throw err;
 
-			const updated = data.replace(/,/g, ',\n');
-			fs.writeFile('./responses/keychain.json', updated, 'utf8', (err) => {
-	 	 		if (err) throw err;
-			});
-		})
-		NeoLog.Debug(`Fetched ${count} New Keychains From Nitestats`)
-	}
+		const updated = data.replace(/,/g, ',\n');
+		fs.writeFile('./responses/keychain.json', updated, 'utf8', (err) => {
+			if (err) throw err;
+		});
+	})
+	NeoLog.Debug(`Fetched ${count} New Keychains From Nitestats`)
 }
 
 async function startbackend(){
