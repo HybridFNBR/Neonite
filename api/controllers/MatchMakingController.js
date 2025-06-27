b769d6dd2569166fd186318a9a2ef8dd468059ed
HybridFNBR <HybridFNBR@.>
2025-06-27 01:09:11 +0100

    revert

--- api/controllers/MatchMakingController.js
+++ api/controllers/MatchMakingController.js
@@ -3,19 +3,18 @@ const {
 } = require('../../structs/errors');
 const NeoLog = require('../../structs/NeoLog')
 const {getVersionInfo} = require("../../config/defs")
-const WebSocket = require('ws');
-const jsonwebtoken = require('jsonwebtoken');
-const crypto = require('crypto');
 
 module.exports = {
     
     matchmakingTicket: function(req, res){
-		var ParsedBckt = {
+		const {version} = getVersionInfo(req);
+        var ParsedBckt = {
 			NetCL: "",
 			Region: "",
 			Playlist: "",
 			HotfixVerion: -1
 		}
+
 		try {
 			var splitted = req.query.bucketId.split(':');
 			ParsedBckt.NetCL = splitted[0];
@@ -31,7 +30,6 @@ module.exports = {
 				throw new ApiException(errors.com.epicgames.fortnite.invalid_bucket_id).withMessage(`Failed to parse bucketId: '${req.query.bucketId}'`).with(req.query.bucketId)
 			}
 		}
-		
 		res.cookie("NetCL", ParsedBckt.NetCL);
 		var data = {
 			"playerId": req.params.accountId,
@@ -50,19 +48,16 @@ module.exports = {
 			"expireAt": new Date().addHours(1),
 			"nonce": RandomString(32)
 		}
-
 		Object.entries(req.query).forEach(([key, value]) => {
 			if (key == "player.subregions" && value.includes(',')) {
 				data.attributes["player.preferredSubregion"] = value.split(',')[0];
 			}
-
 			data.attributes[key] = value;
 		});
-
 		var payload = Buffer.from(JSON.stringify(data, null, 0)).toString('base64');
 		NeoLog.Log(`Matchmaking into ${ParsedBckt.Playlist}`)
 		res.json({
-			"serviceUrl": "ws://localhost:5596",
+			"serviceUrl": "ws://localhost:5595",
 			"ticketType": "mms-player",
 			"payload": payload,
 			"signature": undefined
@@ -78,6 +73,7 @@ module.exports = {
     },
 
     matchmakingSession2: function(req, res){
+        var BuildUniqueId = req.cookies["NetCL"];
 		res.json({
 			"id": req.params.sessionId,
 			"ownerId": "Neonite",
@@ -101,7 +97,7 @@ module.exports = {
 			"usesPresence": false,
 			"allowJoinViaPresence": true,
 			"allowJoinViaPresenceFriendsOnly": false,
-			"buildUniqueId": "00000000",
+			"buildUniqueId": BuildUniqueId || "00000000",
 			"lastUpdated": "2020-11-09T00:40:28.878Z",
 			"started": false
 		});
@@ -138,64 +134,3 @@ function RandomString(length) {
 	}
 	return result.join('');
 }
-
-Date.prototype.addHours = function (h) {
-	this.setTime(this.getTime() + (h * 60 * 60 * 1000));
-	return this;
-}
-
-const wss = new WebSocket.Server({
-	port: 5596
-});
-
-wss.on('connection', function connection(ws) {
-    ws.send(JSON.stringify({
-		"payload": {
-			"state": "Connecting"
-		},
-		"name": "StatusUpdate"
-	}));
-
-	ws.send(JSON.stringify({
-		"payload": {
-			"totalPlayers": 1,
-			"connectedPlayers": 1,
-			"state": "Waiting"
-		},
-		"name": "StatusUpdate"
-	}));
-
-	ws.send(JSON.stringify({
-		"payload": {
-			"ticketId": "NEONITE",
-			"queuedPlayers": 1,
-			"estimatedWaitSec": 1,
-			"status": {},
-			"state": "Queued"
-		},
-		"name": "StatusUpdate"
-	}));
-
-	let matchmakingtoken = jsonwebtoken.sign({
-		"joinDelaySec":4,
-		"iss":"mms",
-		"sessionId":"NEONITE",
-		"exp":9668532724,
-		"env":"prod",
-		"iat":1668529124,
-		"matchId":"NEONITE",
-		"jti":"j1GaITnR4Op4JD6l1lbH9dfbyntYLIYn",
-		"playerId":"198b3f9aa494490a83e8d541622235b0"
-	}, "RS256")
-
-	ws.send(JSON.stringify({
-		"payload": {
-			"matchId": "NEONITE",
-			"sessionId": "NEONITE",
-			"playerId": "198b3f9aa494490a83e8d541622235b0",
-			"joinDelaySec": 4
-		},
-		"payloadJwt": matchmakingtoken,
-		"name": "Play"
-	}))
-});
