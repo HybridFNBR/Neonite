d79c13bef448f3afdd4077b2c47a62617a9583f3
Joshua <68912574+HybridFNBR@users.noreply.github.com>
2025-10-09 19:25:31 +0100

    nitestats -> dillyapis

--- app.js
+++ app.js
@@ -26,19 +26,21 @@ async function compareAndUpdateKeychain() {
         NeoLog.Debug(`Fetched ${missingCount} New Keychains from Fortnite Central.`);
     } 
     else if (response.status === 503 || response.status === 403 | response.status === 404) {
-        NeoLog.Error("Fortnite Central is down, falling back to Nitestats for the keychain");
-        const fallbackResponse = await axios.get('https://api.nitestats.com/v1/epic/keychain');
-        const data = fallbackResponse.data;
-        let count = 0;
+        NeoLog.Error("Fortnite Central is down, falling back to dillyapis for the keychain");
+        const fallbackResponse = await axios.get('https://export-service.dillyapis.com/v1/aes');
+        const data = fallbackResponse.data
+        let missingCount = 0;
+        const keychainArray = [];
 
-        for (const entry of data) {
-            if (!keychain.includes(entry)) {
-                count++;
-                keychain.push(entry);
+        for (const keys of data.dynamicKeys) {
+            if (!keychain.includes(keys.keychain)) {
+                missingCount++;
+                keychainArray.push(keys.keychain);
             }
         }
+        keychain.push(...keychainArray);
         fs.writeFileSync("./responses/keychain.json", JSON.stringify(keychain, null, 2));
-        NeoLog.Debug(`Fetched ${count} New Keychains From Nitestats`);
+        NeoLog.Debug(`Fetched ${missingCount} New Keychains From dillyapis`);
     }
 }
 
