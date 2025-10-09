d1dbf11910ef626e0fd22e7375fa4ba7cdbe780d
Joshua <68912574+HybridFNBR@users.noreply.github.com>
2025-10-09 15:36:21 +0100

    keychain fallback

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const { default: axios } = require("axios");
 const fs = require("fs");
 const ini = require("ini");
 const config = ini.parse(fs.readFileSync("config.ini", "utf-8"));
+const keychain = JSON.parse(fs.readFileSync("./responses/keychain.json", "utf-8"));
 
 async function compareAndUpdateKeychain() {
-    const response = await axios.get("https://fortnitecentral.genxgames.gg/api/v1/aes");
-
+    const response = await axios.get('https://fortnitecentral.genxgames.gg/api/v1/aes', {validateStatus: () => true});    
     if (response.status === 200) {
         const data = response.data;
-        const keychain = JSON.parse(fs.readFileSync("./responses/keychain.json", "utf-8"));
 
         let missingCount = 0;
         const keychainArray = [];
@@ -25,8 +24,21 @@ async function compareAndUpdateKeychain() {
 
         fs.writeFileSync("./responses/keychain.json", JSON.stringify(keychain, null, 2));
         NeoLog.Debug(`Fetched ${missingCount} New Keychains from Fortnite Central.`);
-    } else if (response.status === 503 || response.status === 403) {
-        NeoLog.Error(`Failed to update Keychain, received status: ${response.status}.`);
+    } 
+    else if (response.status === 503 || response.status === 403 | response.status === 404) {
+        NeoLog.Error("Fortnite Central is down, falling back to Nitestats for the keychain");
+        const fallbackResponse = await axios.get('https://api.nitestats.com/v1/epic/keychain');
+        const data = fallbackResponse.data;
+        let count = 0;
+
+        for (const entry of data) {
+            if (!keychain.includes(entry)) {
+                count++;
+                keychain.push(entry);
+            }
+        }
+        fs.writeFileSync("./responses/keychain.json", JSON.stringify(keychain, null, 2));
+        NeoLog.Debug(`Fetched ${count} New Keychains From Nitestats`);
     }
 }
 
