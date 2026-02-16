5a625de613ea499e64c997ee7fdbcda761b03361
Joshua <68912574+HybridFNBR@users.noreply.github.com>
2026-02-16 15:28:12 +0000

    dont hardcode hotfix files and no longer need to verify token every 30 seconds

--- api/controllers/CloudStorageController.js
+++ api/controllers/CloudStorageController.js
@@ -30,116 +30,100 @@ module.exports = {
 
 	},
 
-	defaultGame: function (req, res) {
-		const { version, versionGlobal } = getVersionInfo(req);
-		res.setHeader("content-type", "application/octet-stream")
-		let DefaultGame = fs.readFileSync(path.join(__dirname, '../../hotfixes/DefaultGame.ini'), 'utf-8');
-		if (versionGlobal >= 20) {
-			DefaultGame = DefaultGame.replace(
-				";+CurveTable=/TacticalSprintGame/DataTables/TacticalSprintGameData;RowUpdate;Default.TacticalSprint.Sprint.Energy.CostPerSecond;0.0;0.0",
-				"+CurveTable=/TacticalSprintGame/DataTables/TacticalSprintGameData;RowUpdate;Default.TacticalSprint.Sprint.Energy.CostPerSecond;0.0;0.0"
-			);
-		}
-		const replacements = {
-			"7.30": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Low, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Low, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"7.40": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"8.50": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"8.51": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"9.40": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"9.41": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"10.40": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Highest, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Highest, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"11.30": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Lowest, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Lowest, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"12.41": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"12.61": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Fritter_64, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Fritter_64, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-			"14.60": [
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Junior_32, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
-				"+FrontEndPlaylistData=(PlaylistName=Playlist_Junior_32, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
-			],
-		};
-
-		if (replacements[version]) {
-			const [defaultvalue, replacedValue] = replacements[version];
-			DefaultGame = DefaultGame.replace(defaultvalue, replacedValue);
-		}
-		res.send(DefaultGame)
-	},
-
 	config: function (req, res) {
 		return res.status(204).end()
 	},
 
-	defaultEngine: function (req, res) {
-		res.setHeader("content-type", "application/octet-stream")
+	hotfixFiles: function (req, res) {
+		res.setHeader("content-type", "application/octet-stream");
 		const { version, versionGlobal } = getVersionInfo(req);
-		let DefaultEngine = fs.readFileSync(path.join(__dirname, '../../hotfixes/DefaultEngine.ini'), 'utf-8');
-		if (version === "32.11" || version === "35.20" || version === "37.51" || version === "38.11") {
-			DefaultEngine = DefaultEngine.replace(
-				';Fort.Event.bForceOffLoadingScreen=1',
-				'Fort.Event.bForceOffLoadingScreen=1'
-			);
+		const filePath = path.join(__dirname, '../../hotfixes/', req.params.fileName);
+		let fileContent = fs.readFileSync(filePath, 'utf-8')	
+		switch (req.params.fileName) {
+			case 'DefaultEngine.ini':
+				if (version === "32.11" || version === "35.20" || version === "37.51" || version === "38.11") {
+					fileContent = fileContent.replace(
+						';Fort.Event.bForceOffLoadingScreen=1',
+						'Fort.Event.bForceOffLoadingScreen=1'
+					);
+				}
+			break;
+			case 'DefaultRuntimeOptions.ini':
+				if (version === "26.20") {
+					fileContent = fileContent.replace(
+						';+ExperimentalBucketPercentList=(ExperimentNum=27,Name="ShowMultiProductItemShop",BucketPercents=(100,0,0),WinningBucketIndex=-1)',
+						'+ExperimentalBucketPercentList=(ExperimentNum=27,Name="ShowMultiProductItemShop",BucketPercents=(100,0,0),WinningBucketIndex=-1)'
+					);
+				}
+				else if (version === "17.30") {
+					fileContent = fileContent.replace(
+						'bEnableSocialTab=false',
+						'bEnableSocialTab=true'
+					);
+				}
+			case 'DefaultGame.ini':
+				if (versionGlobal >= 20) {
+					fileContent = fileContent.replace(
+						";+CurveTable=/TacticalSprintGame/DataTables/TacticalSprintGameData;RowUpdate;Default.TacticalSprint.Sprint.Energy.CostPerSecond;0.0;0.0",
+						"+CurveTable=/TacticalSprintGame/DataTables/TacticalSprintGameData;RowUpdate;Default.TacticalSprint.Sprint.Energy.CostPerSecond;0.0;0.0"
+					);
+				}
+				const replacements = {
+					"7.30": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Low, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Low, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"7.40": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"8.50": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"8.51": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Med, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"9.40": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"9.41": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Higher, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"10.40": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Highest, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Highest, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"11.30": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Lowest, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_Lowest, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"12.41": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Music_High, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"12.61": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Fritter_64, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Fritter_64, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+					"14.60": [
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Junior_32, PlaylistAccess=(bEnabled=false, CategoryIndex=1, DisplayPriority=-999))",
+						"+FrontEndPlaylistData=(PlaylistName=Playlist_Junior_32, PlaylistAccess=(bEnabled=true, CategoryIndex=1, DisplayPriority=-999))"
+					],
+				};
+				if (replacements[version]) {
+					const [defaultvalue, replacedValue] = replacements[version];
+					fileContent = fileContent.replace(defaultvalue, replacedValue);
+				}
+			break;
 		}
-		if (versionGlobal == 4) {
-			DefaultEngine = DefaultEngine
-				.replace(';Protocol="http"', 'Protocol="http"')
-				.replace(';Domain="localhost:5595"', 'Domain="localhost:5595"');
-		}
-		res.send(DefaultEngine)
+		
+		res.send(fileContent);
 	},
 
-	defaultRuntimeOptions: function (req, res) {
-		res.setHeader("content-type", "application/octet-stream")
-		const { version } = getVersionInfo(req);
-		let DefaultRuntimeOptions = fs.readFileSync(path.join(__dirname, '../../hotfixes/DefaultRuntimeOptions.ini'), 'utf-8');
-		if (version === "26.20") {
-			DefaultRuntimeOptions = DefaultRuntimeOptions.replace(
-				';+ExperimentalBucketPercentList=(ExperimentNum=27,Name="ShowMultiProductItemShop",BucketPercents=(100,0,0),WinningBucketIndex=-1)',
-				'+ExperimentalBucketPercentList=(ExperimentNum=27,Name="ShowMultiProductItemShop",BucketPercents=(100,0,0),WinningBucketIndex=-1)'
-			);
-		}
-		else if (version === "17.30") {
-			DefaultRuntimeOptions = DefaultRuntimeOptions.replace(
-				'bEnableSocialTab=false',
-				'bEnableSocialTab=true'
-			);
-		}
-		res.send(DefaultRuntimeOptions)
-	},
-
-	defaultInput: function (req, res) {
-		res.setHeader("content-type", "application/octet-stream")
-		let DefaultInput = fs.readFileSync(path.join(__dirname, '../../hotfixes/DefaultInput.ini'), 'utf-8');
-		res.send(DefaultInput)
-	},
 
 	user: async function (req, res) {
 		const accountId = req.params.accountId;
