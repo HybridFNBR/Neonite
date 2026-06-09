cc0814fa1b1245d44bd5dfeb4282efd8e8d5acd6
HybridFNBR <hybridfnbrbusiness@gmail.com>
2026-06-09 14:48:27 +0100

    much cleaner way of doing this then what i was doing

--- api/controllers/DiscoveryController.js
+++ api/controllers/DiscoveryController.js
@@ -1,6 +1,7 @@
 const { getVersionInfo, loadJSON, misc } = require("../../config/defs")
 const discoveryv1 = loadJSON("../discovery/discoveryMenuV1.json");
 const baseDiscoveryV2 = loadJSON("../discovery/discoveryMenuV2.json")
+const { default: axios } = require("axios");
 module.exports = {
 
 	//first interation of discovery api
@@ -540,31 +541,30 @@ module.exports = {
 
 	mnemonicLinks: function (req, res) {
 		const { versionGlobal, version } = getVersionInfo(req);
-		const discoveryv2 = structuredClone(baseDiscoveryV2);
+		global.discoveryv2 = structuredClone(baseDiscoveryV2);
 		if (version >= 23.00) {
 			if (version >= 33.00) {
-				playlistActive(discoveryv2, "set_figment_playlists", true, false)
-				addPlaylistOverwrite(discoveryv2, "figment", version)
+				playlistActive(global.discoveryv2, "set_figment_playlists", true, false)
+				addPlaylistOverwrite(global.discoveryv2, "figment", version)
 			}
-			if (version >= 36.10) { playlistActive(discoveryv2, "set_forbiddenfruit_nobuild_playlists", true, false) }
-			if (version >= 37.31 && version < 40.20) { playlistActive(discoveryv2, "playlist_stridemice", true, false) }
+			if (version >= 36.10) { playlistActive(global.discoveryv2, "set_forbiddenfruit_nobuild_playlists", true, false) }
+			if (version >= 37.31 && version < 40.20) { playlistActive(global.discoveryv2, "playlist_stridemice", true, false) }
 			if (misc.bInEditor === true) { //scuffed way of doing it but works
-				playlistActive(discoveryv2, "playlist_pilgrimquickplay", false, true)
-				playlistActive(discoveryv2, "playlist_juno", false, true)
-				updateMetadata(discoveryv2, "set_br_playlists", {
+				playlistActive(global.discoveryv2, "playlist_pilgrimquickplay", false, true)
+				playlistActive(global.discoveryv2, "playlist_juno", false, true)
+				updateMetadata(global.discoveryv2, "set_br_playlists", {
 					"frontend_plugin": ""
 				});
-				
 			}
 			if (playlistManager[version]) { playlistManager[version].forEach(playlist => playlistActive(discoveryv2, playlist, true, false)) }
 			if (version >= 38.11) {
-				updateMetadata(discoveryv2, "ref_panel_byepicfeeder_1", {
+				updateMetadata(global.discoveryv2, "ref_panel_byepicfeeder_1", {
 					ref_id: "CreativeDiscoverySurface_FrontendV2:ByEpicFeeder"
 				});
-				updateMetadata(discoveryv2, "ref_panel_byepicfeederreload_1", {
+				updateMetadata(global.discoveryv2, "ref_panel_byepicfeederreload_1", {
 					ref_id: "CreativeDiscoverySurface_FrontendV2:ByEpicConvergenceReload"
 				});
-				updateMetadata(discoveryv2, "ref_panel_byepicfeedermoremodes_1", {
+				updateMetadata(global.discoveryv2, "ref_panel_byepicfeedermoremodes_1", {
 					ref_id: "CreativeDiscoverySurface_FrontendV2:ByEpicConvergenceMoreModes"
 				});
 			}
@@ -613,17 +613,11 @@ module.exports = {
 				});
 			}
 			if (versionGlobal == 40) {
-				updateMetadata(discoveryv2, "set_br_playlists", {
+				updateMetadata(global.discoveryv2, "set_br_playlists", {
 					"matchmaking_plugins": [
 						"HazelnutSpread_RivalsFrontend"
 					]
 				});
-			}
-			if (misc.bInEditor === true) {
-				updateMetadata(discoveryv2, "set_br_playlists", {
-					"frontend_plugin": ""
-				});
-				
 			}
 			return res.json(discoveryv2);
 		}
@@ -633,87 +627,31 @@ module.exports = {
 		}
 	},
 
-	related: function (req, res) {
+	related: async function (req, res) {
 		const { version, versionGlobal } = getVersionInfo(req);
-		const discoveryv2 = structuredClone(baseDiscoveryV2);
 		const relatedResponse = {
 			parentLinks: [],
 			links: {}
 		}
-		if (version >= 33.00) {
-			playlistActive(discoveryv2, "set_figment_playlists", true, false)
-			addPlaylistOverwrite(discoveryv2, "figment", version)
-		}
-		if (version >= 36.10) { playlistActive(discoveryv2, "set_forbiddenfruit_nobuild_playlists", true, false) }
-		if (version >= 37.31) { playlistActive(discoveryv2, "playlist_stridemice", true, false) }
-		if (playlistManager[version]) { playlistManager[version].forEach(playlist => playlistActive(discoveryv2, playlist, true, false)) }
-		if (versionGlobal >= 39) {
-			updateMetadata(discoveryv2, "playlist_defaultsolo", {
-				"alt_title": { "en": "CH7 BR Map", "de": "CH7 BR Kaart", "ru": "CH7 BR Карта", "ko": "CH7 BR 지도", "pt-BR": "Mapa CH7 BR", "it": "CH7 Mappa BR", "fr": "CH7 Carte BR", "zh-CN": "", "es": "CH7 Mapa BR", "es-MX": "CH7 Mapa BR", "zh": "", "ar": "CH7 BR خريطة", "zh-Hant": "", "ja": "CH7 BR マップ", "pl": "CH7 Mapa BR", "es-419": "CH7 Mapa BR", "tr": "CH7 BR Haritası" }, 	//used DeepL for translations they may not be correct.
-				"title": "CH7 BR Map",
-				"image_urls": {
-					"url_s": "https://cms-assets.unrealengine.com/cm6l5gfpm05kr07my04cqgy2x/cmigg1t1l089n07nam0q9jd8b",
-					"url_xs": "https://cms-assets.unrealengine.com/cm6l5gfpm05kr07my04cqgy2x/cmigg1t1l089n07nam0q9jd8b",
-					"url_m": "https://cms-assets.unrealengine.com/cm6l5gfpm05kr07my04cqgy2x/cmigg1t1l089n07nam0q9jd8b",
-					"url": "https://cms-assets.unrealengine.com/cm6l5gfpm05kr07my04cqgy2x/cmigg1t1l089n07nam0q9jd8b"
-				},
-			});
+		if (!global.discoveryv2) {await axios.post('http://localhost:5595/links/api/fn/mnemonic', {},{ headers: {'User-Agent': req.headers['user-agent'] }});
+		} //incase someone decides to restart neonite for any reason
 
-			if (versionGlobal === 39) {
-				updateMetadata(discoveryv2, "playlist_defaultduo", {
-					"alt_title": { "en": "CH6 BR Map", "de": "CH6 BR Kaart", "ru": "CH6 BR Карта", "ko": "CH6 BR 지도", "pt-BR": "Mapa CH7 BR", "it": "CH6 Mappa BR", "fr": "CH6 Carte BR", "zh-CN": "", "es": "CH6 Mapa BR", "es-MX": "CH6 Mapa BR", "zh": "", "ar": "CH6 BR خريطة", "zh-Hant": "", "ja": "CH6 BR マップ", "pl": "CH6 Mapa BR", "es-419": "CH6 Mapa BR", "tr": "CH6 BR Haritası" },//used DeepL for translations they may not be correct.
-					"title": "CH6 BR Map",
-					"image_url": "https://cdn2.unrealengine.com/fortnite-battle-royale-chapter-6-season-1-hunters-modern-1920x1080-8ec1a8791b58.jpg",
-					"image_urls": {
-						"url_s": "https://cdn2.unrealengine.com/fortnite-battle-royale-chapter-6-season-1-hunters-modern-1920x1080-8ec1a8791b58.jpg",
-						"url_xs": "https://cdn2.unrealengine.com/fortnite-battle-royale-chapter-6-season-1-hunters-modern-1920x1080-8ec1a8791b58.jpg",
-						"url_m": "https://cdn2.unrealengine.com/fortnite-battle-royale-chapter-6-season-1-hunters-modern-1920x1080-8ec1a8791b58.jpg",
-						"url": "https://cdn2.unrealengine.com/fortnite-battle-royale-chapter-6-season-1-hunters-modern-1920x1080-8ec1a8791b58.jpg"
-					},
-					"lobby_background_image_urls": {
-						"url": "https://cdn2.unrealengine.com/mkart-ch6s4-lobby-bg-4096x2048-d73f821bb40c.jpg"
-					},
-				});
-			}
-
-			updateMetadata(discoveryv2, "playlist_trios", {
-				"alt_title": { "en": "Simpsons BR Map", "de": "Simpsons BR Kaart", "ru": "Simpsons BR Карта", "ko": "Simpsons BR 지도", "pt-BR": "Mapa Simpsons BR", "it": "Simpsons Mappa BR", "fr": "Simpsons Carte BR", "zh-CN": "", "es": "Simpsons Mapa BR", "es-MX": "Simpsons Mapa BR", "zh": "", "ar": "Simpsons BR خريطة", "zh-Hant": "", "ja": "Simpsons BR マップ", "pl": "Simpsons Mapa BR", "es-419": "Simpsons Mapa BR", "tr": "Simpsons BR Haritası" }, 	//used DeepL for translations they may not be correct.
-				"title": "Simpsons BR Map",
-				"image_url": "https://cdn2.unrealengine.com/fortnite-x-the-simpsons-7-3840x2160-5e88ce59a64c.png",
-				"image_urls": {
-					"url_s": "https://cdn2.unrealengine.com/fortnite-x-the-simpsons-7-3840x2160-5e88ce59a64c.png",
-					"url_xs": "https://cdn2.unrealengine.com/fortnite-x-the-simpsons-7-3840x2160-5e88ce59a64c.png",
-					"url_m": "https://cdn2.unrealengine.com/fortnite-x-the-simpsons-7-3840x2160-5e88ce59a64c.png",
-					"url": "https://cdn2.unrealengine.com/fortnite-x-the-simpsons-7-3840x2160-5e88ce59a64c.png"
-				},
-				"lobby_background_image_urls": {
-					"url": "https://cdn2.unrealengine.com/mkart-ch6ms2-lobbybg-plate-4098x2048-c51669ab5daa.jpg"
-				},
-			});
-		}
-		if (versionGlobal === 40) {
-			updateMetadata(discoveryv2, "set_br_playlists", {
-				"matchmaking_plugins": [
-					"HazelnutSpread_RivalsFrontend"
-				]
-			});
-		}
-		const findPlaylist = discoveryv2.findIndex(i => i.mnemonic === req.params.playlistId);
-		if (discoveryv2[findPlaylist].metadata["sub_link_codes"]) {
-			relatedResponse.parentLinks.push(discoveryv2[findPlaylist])
-			for (const subLinkCode of discoveryv2[findPlaylist].metadata.sub_link_codes) {
-				relatedResponse.links[subLinkCode] = discoveryv2.find(i => i.mnemonic === subLinkCode);
+		const findPlaylist = global.discoveryv2.findIndex(i => i.mnemonic === req.params.playlistId);
+		if (global.discoveryv2[findPlaylist].metadata["sub_link_codes"]) {
+			relatedResponse.parentLinks.push(global.discoveryv2[findPlaylist])
+			for (const subLinkCode of global.discoveryv2[findPlaylist].metadata.sub_link_codes) {
+				relatedResponse.links[subLinkCode] = global.discoveryv2.find(i => i.mnemonic === subLinkCode);
 			}
 		}
 		else {
-			relatedResponse.links[discoveryv2[findPlaylist].mnemonic] = discoveryv2[findPlaylist]
-			if (discoveryv2[findPlaylist].metadata.parent_set) {
-				const parentSet = discoveryv2.find(i => i.mnemonic === discoveryv2[findPlaylist].metadata.parent_set);
+			relatedResponse.links[global.discoveryv2[findPlaylist].mnemonic] = global.discoveryv2[findPlaylist]
+			if (global.discoveryv2[findPlaylist].metadata.parent_set) {
+				const parentSet = global.discoveryv2.find(i => i.mnemonic === global.discoveryv2[findPlaylist].metadata.parent_set);
 				if (parentSet) {
 					relatedResponse.parentLinks.push(parentSet)
 					const existingLinks = new Set(Object.keys(relatedResponse.links));
 					parentSet.metadata["sub_link_codes"].forEach(code => {
-						const matchingResult = discoveryv2.find(i => i.mnemonic === code);
+						const matchingResult = global.discoveryv2.find(i => i.mnemonic === code);
 						if (matchingResult && !existingLinks.has(code)) {
 							relatedResponse.links[code] = matchingResult;
 						}
@@ -721,7 +659,6 @@ module.exports = {
 				}
 			}
 		}
-		addPlaylistOverwrite(relatedResponse.parentLinks, "figment", version);
 		res.json(relatedResponse);
 	},
 
